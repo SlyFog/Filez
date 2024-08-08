bc251c10664aab613fae36e4e83568cf50506f2f
SlyFog <corbinmwest@outlook.com>
2024-08-08 03:05:36 -0500

    Create system_info.sh

---
+++ system_info.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Current User @ Hostname:"
+echo "$(whoami) @ $(hostname)"
+
+echo "CPU Info:"
+lscpu | grep "Model name"
+lscpu | grep "CPU MHz"
+
+echo "Memory Info:"
+free -h | grep "Mem"
+
+echo "Swap Info:"
+free -h | grep "Swap"
+
+echo "Disk Space Info:"
+df -hT | grep -E "ext4|xfs"
+
+echo "Network Info:"
+ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}/\d+'
